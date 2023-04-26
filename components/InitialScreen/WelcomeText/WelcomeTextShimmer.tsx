cdca8d9f7b1c599317db7d760cb499663e58891a
eXebyss <mysticlord27@gmail.com>
2023-04-27 01:00:38 +0300

    upd: WelcomeTextShimmer

--- components/InitialScreen/WelcomeText/WelcomeTextShimmer.tsx
+++ components/InitialScreen/WelcomeText/WelcomeTextShimmer.tsx
@@ -3,8 +3,8 @@ const WelcomeTextShimmer = () => {
         <div className="p-4 w-full mx-auto">
             <div className="animate-pulse flex space-x-4">
                 <div className="flex-1 space-y-6 py-1">
-                    <div className="grid grid-row-3 lg:grid-rows-1 grid-cols-1 lg:grid-cols-3 gap-y-8 lg:gap-y-0 lg:gap-x-8">
-                        <div className="h-24 bg-neutral rounded col-span-1" />
+                    <div className="grid grid-row-2 lg:grid-rows-1 grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-y-0 lg:gap-x-8">
+                        <div className="h-24 bg-neutral rounded col-span-2 lg:col-span-1" />
                         <div className="h-24 bg-neutral rounded col-span-1" />
                         <div className="h-24 bg-neutral rounded col-span-1" />
                     </div>
