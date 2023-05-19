c08a901e62f6456a2ba529cdfee401c0f5155ab4
eXebyss <mysticlord27@gmail.com>
2023-05-19 21:30:49 +0300

    feat: add/enable PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/no-invalid-html-attribute */
 import { Coda } from 'next/font/google';
 import Footer from '@/components/UI/Footer/Footer';
 import '../styles/global/global.scss';
 import { AuthContextProvider, FirestoreContextProvider } from '@/context';
+import { ogUrl, siteDescription, siteTitle } from '@/constants';
 
 const coda = Coda({
     weight: '400',
@@ -17,7 +19,187 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" data-theme="light" className={`${coda.variable}`}>
-            <head />
+            <head>
+                <title>{`${siteTitle}: Home`}</title>
+                <meta
+                    content="width=device-width, initial-scale=1"
+                    name="viewport"
+                />
+                <meta name="description" content={`${siteDescription}`} />
+                <meta charSet="utf-8" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="manifest" href="/manifest.webmanifest" />
+                <link rel="apple-touch-icon" href="/icon.png" />
+                <meta name="theme-color" content="#f2f2f2" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2048-2732.jpg"
+                    media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2732-2048.jpg"
+                    media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1668-2388.jpg"
+                    media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2388-1668.jpg"
+                    media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1536-2048.jpg"
+                    media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2048-1536.jpg"
+                    media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1668-2224.jpg"
+                    media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2224-1668.jpg"
+                    media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1620-2160.jpg"
+                    media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2160-1620.jpg"
+                    media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1290-2796.jpg"
+                    media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2796-1290.jpg"
+                    media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1179-2556.jpg"
+                    media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2556-1179.jpg"
+                    media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1284-2778.jpg"
+                    media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2778-1284.jpg"
+                    media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1170-2532.jpg"
+                    media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2532-1170.jpg"
+                    media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1125-2436.jpg"
+                    media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2436-1125.jpg"
+                    media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1242-2688.jpg"
+                    media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2688-1242.jpg"
+                    media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-828-1792.jpg"
+                    media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1792-828.jpg"
+                    media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1242-2208.jpg"
+                    media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-2208-1242.jpg"
+                    media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-750-1334.jpg"
+                    media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1334-750.jpg"
+                    media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-640-1136.jpg"
+                    media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+                />
+                <link
+                    rel="apple-touch-startup-image"
+                    href="apple-splash-1136-640.jpg"
+                    media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+                />
+                <meta property="og:title" content={`${siteTitle}`} />
+                <meta property="og:image" content={`${ogUrl}logo.png`} />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="628" />
+                <meta
+                    property="og:description"
+                    content={`${siteDescription}`}
+                />
+                <meta property="og:url" content={`${ogUrl}`} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content={`${siteTitle}`} />
+                <meta property="twitter:image" content={`${ogUrl}logo.png`} />
+                <meta
+                    name="twitter:description"
+                    content={`${siteDescription}`}
+                />
+                <meta property="twitter:site" content={`${ogUrl}`} />
+            </head>
             <body>
                 <AuthContextProvider>
                     <FirestoreContextProvider>
--- next.config.js
+++ next.config.js
@@ -1,8 +1,47 @@
+const path = require('path');
+const withPWAInit = require('next-pwa');
+
+/** @type {import('next-pwa').PWAConfig} */
+const withPWA = withPWAInit({
+    dest: 'public',
+    // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
+    buildExcludes: ['app-build-manifest.json'],
+    register: true,
+    skipWaiting: true,
+});
+
+const generateAppDirEntry = (entry) => {
+    const packagePath = require.resolve('next-pwa');
+    const packageDirectory = path.dirname(packagePath);
+    const registerJs = path.join(packageDirectory, 'register.js');
+
+    return entry().then((entries) => {
+        // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
+        if (entries['main-app'] && !entries['main-app'].includes(registerJs)) {
+            if (Array.isArray(entries['main-app'])) {
+                entries['main-app'].unshift(registerJs);
+            } else if (typeof entries['main-app'] === 'string') {
+                // eslint-disable-next-line no-param-reassign
+                entries['main-app'] = [registerJs, entries['main-app']];
+            }
+        }
+        return entries;
+    });
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     experimental: {
         appDir: true,
     },
+    webpack: (config) => {
+        const entry = generateAppDirEntry(config.entry);
+        // eslint-disable-next-line no-param-reassign
+        config.entry = () => entry;
+
+        return config;
+    },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
