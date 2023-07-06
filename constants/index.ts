066fcf26255bbb0d3190fbbb5c6c1b991d3c2535
eXebyss <mysticlord27@gmail.com>
2023-07-06 20:02:32 +0300

    chore: improve MegaMenu

--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,4 @@ export { default as siteTitle } from './siteTitle';
 export { default as ogUrl } from './ogUrl';
 export { default as siteDescription } from './siteDescription';
 export { linkedinUrl, githubUrl, codewarsUrl } from './socialUrl';
+export { default as LINKS } from './links';
---
+++ constants/links.ts
@@ -0,0 +1,11 @@
+const LINKS = [
+    { href: '/', text: 'Home 🏠' },
+    { href: '/my-account', text: 'Account' },
+    { href: '/naptime', text: 'Nap History' },
+    { href: '/installation-guide', text: 'Installation Guide' },
+    { href: '/privacy-policy', text: 'Privacy Policy' },
+    { href: '/terms-of-service', text: 'Terms of Service' },
+    { href: 'https://www.mihailsfjodorovs.com/#contact', text: 'Contact' },
+];
+
+export default LINKS;
